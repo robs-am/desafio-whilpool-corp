d1c2da0a1375b86ec1992ad9662ff7f4ca3a4091
Roberta <roberta.amarto@ed3.com.br>
2024-08-31 17:35:49 -0300

    feat: inicia componente product

---
+++ react/components/ProductSummary/ProductSummary.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProductSummary = () => {
+  return (
+    <section className='brastemp-purchase'>
+      <div className='brastemp-purchase-description'>
+
+      </div>
+      <div className='brastemp-purchase-box'>
+      <h4 className='brastemp-purchase-title'>Fogão Brastemp 5 Bocas Inox com Turbo Chama</h4>
+        <p className='brastemp-purchase-price'>R$ 1.299,00</p>
+        <p className='brastemp-purchase-installments'>10x de R$ 129,90 sem juros</p>
+        <button className='brastemp-purchase-button'>Comprar</button>
+      </div>
+
+    </section>
+  )
+}
+
+export default ProductSummary
